abaac84247622082e5799fd1ac80e37151e84bb0
Anh Le <32230670+anhle697@users.noreply.github.com>
2018-04-15 18:48:12 -0500

    Current Progress

---
+++ ProjectManagementSystem/Controllers/User_LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManagementSystem.Models;
+
+namespace ProjectManagementSystem.Controllers
+{
+    public class User_LoginController : Controller
+    {
+        private ProjectManagementSystemEntities db = new ProjectManagementSystemEntities();
+
+        // Go to Login Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+       
+}
